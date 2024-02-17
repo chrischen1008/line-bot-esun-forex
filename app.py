9b55852873da4ee9f216a24c914471a6c2049afa
Chris <chris@cotex.com.tw>
2024-02-17 14:32:29 +0800

    update .gitignore

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 configuration = Configuration(access_token=config('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(config('CHANNEL_SECRET'))
 
-
+print('')
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
